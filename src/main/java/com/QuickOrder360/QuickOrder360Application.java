5ba9ba26a06a1715e5f7c5a68587a1ff6dc65c73
LeonGarridoFuentes <le.garridof@duocuc.cl>
2026-05-06 18:26:14 -0400

    primer commit
    subir proyecto

---
+++ src/main/java/com/QuickOrder360/QuickOrder360Application.java
@@ -0,0 +1,13 @@
+package com.QuickOrder360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickOrder360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickOrder360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/QuickOrder360/QuickOrder360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuickOrder360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickOrder360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
